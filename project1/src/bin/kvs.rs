62559676cbb3c5fb622b4fd0f270f007cb3dca83
huangyongquan.dec26 <huangyongquan.dec26@bytedance.com>
2022-09-03 12:51:56 +0800

    project1 done

--- project1/src/bin/kvs.rs
+++ project1/src/bin/kvs.rs
@@ -1,3 +1,53 @@
+use clap::{Args, Parser, Subcommand};
+use std::process::exit;
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Set(Set),
+    Get(Get),
+    Rm(Rm),
+}
+
+#[derive(Args)]
+struct Set {
+    #[clap(value_parser)]
+    key: Option<String>,
+    value: Option<String>,
+}
+
+#[derive(Args)]
+struct Get {
+    #[clap(value_parser)]
+    key: Option<String>,
+}
+
+#[derive(Args)]
+struct Rm {
+    #[clap(value_parser)]
+    key: Option<String>,
+}
+
 fn main() {
-    println!("hello, world!");
+    let args = Cli::parse();
+    match &args.command {
+        Command::Set(set) => {
+            eprintln!("unimplemented");
+            exit(-1);
+        }
+        Command::Get(get) => {
+            eprintln!("unimplemented");
+            exit(-1);
+        }
+        Command::Rm(rm) => {
+            eprintln!("unimplemented");
+            exit(-1);
+        }
+    }
 }
